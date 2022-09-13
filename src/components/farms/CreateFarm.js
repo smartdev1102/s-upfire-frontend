ddf818e5a2bd11fad6ed0c8fd114706e6ca696c5
cryptowarrier <cryptomusha@gmail.com>
2022-09-13 23:48:58 +0900

    updated

--- src/components/farms/CreateFarm.js
+++ src/components/farms/CreateFarm.js
@@ -11,6 +11,7 @@ import { constants } from 'ethers';
 import { Close } from '@mui/icons-material';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 import PerfectScrollbar from 'react-perfect-scrollbar'
+import RoundButton from '../common/RoundButton';
 
 const CreateFarm = ({ open, onClose, create, walletAddress, chain, pairs }) => {
   const [startDate, setstartDate] = useState(new Date());
@@ -23,7 +24,8 @@ const CreateFarm = ({ open, onClose, create, walletAddress, chain, pairs }) => {
   const [rewardBlock, setRewardBlock] = useState('0');
   const [isBonus, setIsBonus] = useState(false);
   const [isV3, setIsV3] = useState(false);
-
+  const [currentSwap, setCurrentSwap] = useState(0); // for avalanche
+  
   const createFarm = () => {
     const startBlock = Math.floor(new Date(startDate).getTime() / 1000);
     const bonusEndBlock = Math.floor(new Date(bonusEndDate).getTime() / 1000);
@@ -98,6 +100,22 @@ const CreateFarm = ({ open, onClose, create, walletAddress, chain, pairs }) => {
         >
           <Box sx={{ width: '100%', height: '100%' }}>
             <PerfectScrollbar style={{ padding: '30px' }}>
+              {
+                (chain === 43113) && (
+                  <Box>
+                    <RoundButton onClick={() => {setCurrentSwap(0); setLpToken('')}} color={currentSwap !== 0 ? 'secondary' : 'primary'} variant='contained'>Pangolin</RoundButton>
+                    <RoundButton onClick={() => { setCurrentSwap(1); setLpToken('') }} color={currentSwap === 1 ? 'primary' : 'secondary'} variant='contained'>Trader Joe</RoundButton>
+                  </Box>
+                )
+              }
+              {
+                (chain === 4) && (
+                  <Box>
+                    <RoundButton onClick={() => setIsV3(false)} color={isV3 ? 'secondary' : 'primary'} variant='contained'>UniswapV2</RoundButton>
+                    <RoundButton onClick={() => { setIsV3(true); setLpToken('') }} color={isV3 ? 'primary' : 'secondary'} variant='contained'>UniswapV3</RoundButton>
+                  </Box>
+                )
+              }
               <Box>
                 <Typography variant='h6' component='h6'>
                   Farm which token?
@@ -111,9 +129,6 @@ const CreateFarm = ({ open, onClose, create, walletAddress, chain, pairs }) => {
               </Box>
               <Box sx={{ color: 'text.secondary' }}>
                 Bonus end date
-              </Box>
-              <Box sx={{mt: '30px'}}>
-                <FormControlLabel control={<Checkbox checked={isV3} onChange={() => {setIsV3(!isV3); setLpToken('')}} />} label="uniswapV3 pool" />
               </Box>
               <Box >
                 <Typography variant='h6' component='h6'>
