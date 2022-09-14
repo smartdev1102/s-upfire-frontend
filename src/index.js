3eabdb67ff150bdbf809c162a29ab664325705d1
cryptowarrier <cryptomusha@gmail.com>
2022-09-15 06:00:57 +0900

    updated

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Provider } from 'react-redux';
 import { theme } from './style/theme';
-import "./index.css"
+import "./index.css";
+import store from './reducers/store';
 
 const myTheme = createTheme(theme);
 
@@ -12,7 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={myTheme}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
